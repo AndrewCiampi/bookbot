c85e59f6ffcb2fd5c25d6b19858a4376bb6db517
Andrew <ajciampi@icloud.com>
2024-10-16 19:33:29 -0700

    Python Word Counter

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+    string_Frank = (len(text.split()))
+    print(f"Word count is: {string_Frank}")
+    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
