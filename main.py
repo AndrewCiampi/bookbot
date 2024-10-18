612100607c1420524763cd7a71722c7dea2ed1f7
Andrew <ajciampi@icloud.com>
2024-10-17 20:04:14 -0700

    Python create report

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    
     num_words = get_num_words(text)
-    print(num_words)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+   
+
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
     
 def get_num_words(text):
     words = text.split()
@@ -24,5 +36,16 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 main()
